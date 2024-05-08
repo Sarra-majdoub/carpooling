4d34d3f4d8e68f6b66c944ca82a055dc646d91d0
Ala-ADN <alaadnzaw@gmail.com>
2024-05-08 18:35:27 +0100

    email controller done

--- src/Controller/EmailController.php
+++ src/Controller/EmailController.php
@@ -8,14 +8,30 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Mime\Email;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\BrowserKit\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
+/**
+ * @Route("/email")
+ */
 class EmailController extends AbstractController
 {
-    /**
-     * @Route("/send-report-email", name="send_report_email")
-     */
-    public function sendReportEmail(MailerInterface $mailer, User $user): Response
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
     {
+        $this->entityManager = $entityManager;
+    }
+
+    /**
+     * @Route("/send-report-email", name="send_report_email", methods={"POST"})
+     */
+    public function sendReportEmail(MailerInterface $mailer, Request $request): JsonResponse
+    {
+        $data = json_decode($request->getContent(), true);
+        $id = $data['id'];
+        $user = $this->entityManager->getRepository(User::class)->find($id);
         $email = (new Email())
             ->from('alaadnzaw@gmail.com')
             ->to('mkchfthnity@gmail.com')
@@ -24,6 +40,6 @@ class EmailController extends AbstractController
 
         $mailer->send($email);
 
-        return new Response('Report email sent successfully.');
+        return new JsonResponse(['message' => 'Email sent']);
     }
 }
