8b34e9c270010d0308def443b18f15cdc05b0f5e
erequem <ramigharbi155@gmail.com>
2024-05-08 19:15:40 +0200

    added the front not working yet

---
+++ Client/src/AdminDashbordComponents/AdminDashbord.js
@@ -0,0 +1,13 @@
+import React from "react";
+import NavbarAdminDashbord from "./NavbarAdminDashbord";
+import Users from "./Users";
+function AdminDashbord() {
+  return (
+    <div>
+      <NavbarAdminDashbord />
+      <Users />
+    </div>
+  );
+}
+
+export default AdminDashbord;
---
+++ Client/src/FrontPageComponents/NavBar.js
@@ -0,0 +1,54 @@
+import React from "react";
+import "./NavBar.css";
+import RegisterHeader from "../ComponentsRegister/RegisterHeader";
+import { useDispatch } from "react-redux";
+import { setFrom, setTo, setDate } from "../reducers/filtersReducer";
+import Logout from "./Logout";
+const NavBar = () => {
+  const dispatch = useDispatch();
+  return (
+    <div className="nav-bar">
+      <RegisterHeader className="mt-1 navbar-img" />
+      <div className="navbar-div">
+        <div className="navbar-div">
+          <label className="nav-bar-label mt-1">From</label>
+          <input
+            type="text"
+            id="fromCity"
+            name="fromCity"
+            placeholder="City"
+            className="mt-1 navbar-input"
+            onChange={(e) => {
+              dispatch(setFrom(e.target.value));
+              console.log(e.target.value);
+            }}
+          />
+        </div>
+        <div className="navbar-div">
+          <label className="nav-bar-label mt-1">To</label>
+          <input
+            type="text"
+            id="toCity"
+            name="toCity"
+            placeholder="City"
+            className="mt-1 navbar-input"
+            onChange={(e) => dispatch(setTo(e.target.value))}
+          />
+        </div>
+      </div>
+      <div className="navbar-div">
+        <label className="nav-bar-label mt-2">On</label>
+        <input
+          type="date"
+          id="date"
+          name="date"
+          className="mt-1 navbar-input"
+          onChange={(e) => dispatch(setDate(e.target.value))}
+        />
+      </div>
+      <Logout />
+    </div>
+  );
+};
+
+export default NavBar;
